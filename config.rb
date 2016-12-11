b203d2115b2ab2d815fe570587da6e7669b4533d
fernahh <fernahh@gmail.com>
2016-12-11 20:21:34 -0200

    Remove unecessary sections

--- config.rb
+++ config.rb
@@ -1,16 +1,14 @@
-# Blog
 activate :blog do |blog|
-  blog.permalink = "{title}"
-  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
-  blog.layout = "layout"
+  blog.permalink = '{title}'
+  blog.sources = 'posts/{year}-{month}-{day}-{title}.html'
+  blog.layout = 'layout'
   blog.summary_length = 250
-  blog.default_extension = ".md"
+  blog.default_extension = '.md'
   blog.paginate = true
   blog.per_page = 10
-  blog.page_link = "blog/{num}"
+  blog.page_link = 'blog/{num}'
 end
 
-# Livereload
 configure :development do
   activate :livereload do
     set :host, '0.0.0.0'
@@ -18,19 +16,16 @@ configure :development do
   end
 end
 
-# Assets
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
-sprockets.append_path File.join root.to_s, "source/assets/components"
+sprockets.append_path File.join root.to_s, 'source/assets/components'
 
 activate :directory_indexes
 
-# Globals
 set :title, '@fernahh | desenvolvedor e de buenas na web'
 set :description, 'Coisas que eu, @fernahh, quero que todos saibam, seja sobre música, código, política ou café.'
 
-# Build
 set :build_dir, '../fernahh.github.io'
 
 configure :build do
