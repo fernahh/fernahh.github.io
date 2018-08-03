0b88870c33bc058584d7cf522d4dc1256f4f8408
fernahh <fernahh@gmail.com>
2018-08-03 10:30:39 -0300

    Create tweet button

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,12 @@
 //= require components/links
+
+document.addEventListener('DOMContentLoaded', function() {
+  var tweetButton = document.querySelector('.Share_Bar__Twitter')
+  var TWITTER_URL = 'https://twitter.com/intent/tweet?text='
+  var postURL = encodeURIComponent(location.href)
+  var postTitle = document.querySelector('.Article__Title').textContent
+  var signature = 'by @fernahh'
+  var tweetContent = TWITTER_URL + postTitle + ' ' + signature + ' ' + postURL
+
+  tweetButton.setAttribute('href', tweetContent)
+})
