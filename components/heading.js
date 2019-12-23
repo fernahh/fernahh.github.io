482bfd6619a63ea0053efc9bb4f1b288404e3a4b
fernahh <fernahh@gmail.com>
2019-12-23 11:59:18 -0300

    Create heading component

---
+++ components/heading.js
@@ -0,0 +1,29 @@
+import { Fragment } from 'react'
+import { useTheme } from '../hooks/useTheme'
+
+export const H1 = ({ children }) => {
+  const { sizes } = useTheme()
+  return (
+    <Fragment>
+      <h1>
+        {children}
+
+        <style jsx>{`
+          h1 {
+            margin-top: ${sizes.medium};
+            margin-bottom: ${sizes.medium};
+            font-weight: 700;
+            font-size: 52px;
+            line-height: 1em;
+          }
+
+          @media screen and (min-width: 768px) {
+            h1 {
+              font-size: 64px;
+            }
+          }
+        `}</style>
+      </h1>
+    </Fragment>
+  )
+}
