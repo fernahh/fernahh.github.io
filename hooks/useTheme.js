2e0301ca8de061ae0143f3339fedb3730f959017
fernahh <fernahh@gmail.com>
2019-12-23 11:58:44 -0300

    Create theme hook

---
+++ hooks/useTheme.js
@@ -0,0 +1,14 @@
+export const useTheme = () => ({
+  colors: {
+    grey: '#999999',
+    link: '#316FEA',
+    linkHover: '#0445c5'
+  },
+  sizes: {
+    extraSmall: '8px',
+    small: '16px',
+    medium: '24px',
+    large: '32px',
+    extraLarge: '40px'
+  }
+})
