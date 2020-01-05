200c6d65c515bbea9d9b64f2e8f896e9b074ca5e
fernahh <fernahh@gmail.com>
2020-01-05 20:28:55 -0300

    Create post content section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { Banner } from '../components/banner'
+import { PostContentSection } from '../components/post-content-section'
 import { Page } from '../layouts/page'
 
 const Index = () => (
   <Page>
     <Banner />
+    <PostContentSection />
   </Page>
 )
 
