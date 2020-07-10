e370d7ac0eb8a14d8e78b5cf9de249a7eb90d636
fernahh <fernahh@gmail.com>
2020-07-10 15:30:45 -0300

    Add favicon

--- components/meta.js
+++ components/meta.js
@@ -4,6 +4,10 @@ export const Meta = () => (
   <Head>
     <title>Fernando Rodrigues</title>
     <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿</text></svg>"
+    />
+    <link
       href="https://fonts.googleapis.com/css?family=Assistant:400,600,700,800&display=swap"
       rel="stylesheet"
     />
