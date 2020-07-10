a30286e7d9a6ffa7dc105e5a1fe79dbe8e8a3871
fernahh <fernahh@gmail.com>
2020-07-10 15:31:33 -0300

    Add font and viewport values to theme

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -2,11 +2,23 @@ export const useTheme = () => ({
   colors: {
     black: '#000000',
     grey: '#999999',
+    greySemiLight: '#e4e4e4',
     greyLight: '#f4f4f4',
     link: '#316FEA',
     linkHover: 'rgba(49, 111, 234, 0.75)',
-    primary: '#52a385',
+    primary: '#00a699',
     white: '#ffffff'
+  },
+  fontSizes: {
+    extraSmall: '14px',
+    small: '16px',
+    base: '20px'
+  },
+  fontWeights: {
+    regular: 400,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800
   },
   radius: {
     small: '4px'
@@ -16,6 +28,14 @@ export const useTheme = () => ({
     small: '16px',
     medium: '24px',
     large: '32px',
-    extraLarge: '40px'
+    extraLarge: '40px',
+    dubleExtraLarge: '80px'
+  },
+  viewport: {
+    maxWidth: '970px'
+  },
+  mediaQueries: {
+    medium: '768px',
+    large: '940px'
   }
 })
