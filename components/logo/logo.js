93a5f577c12d0a93116cbd26f1bf8c0bece4a979
fernahh <fernahh@gmail.com>
2020-11-27 20:03:27 -0300

    Add link on logo

--- components/logo/logo.js
+++ components/logo/logo.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'components/link'
 import styles from './logo.module.css'
 
 export const Logo = () => (
   <div className={styles.logo}>
-    <h1>
-      fernando<span>rodrigues</span>
-    </h1>
+    <Link href="/">
+      <h1>
+        fernando<span>rodrigues</span>
+      </h1>
+    </Link>
   </div>
 )
