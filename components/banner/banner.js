ad659a064970a6a4d6f93a2b2a8d024dc6457664
fernahh <fernahh@gmail.com>
2020-11-27 19:00:12 -0300

    Add banner links

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,3 +1,4 @@
+import { ArrowLink } from 'components/arrow-link'
 import { Link } from 'components/link'
 import { P } from 'components/paragraph'
 import styles from './banner.module.css'
@@ -8,15 +9,23 @@ export const Banner = () => (
       <P>Olá! 👋</P>
       <P>
         Me chamo Fernando Rodrigues. Sou desenvolvedor e atualmente moro em São
-        Paulo, SP. Atualmente trabalho no time design system no{' '}
-        <Link
-          href="https://www.quintoandar.com.br/"
-          title="Site da QuintoAndar"
-        >
-          QuintoAndar
-        </Link>
-        .
+        Paulo, SP. Atualmente trabalho no time design system no QuintoAndar.
       </P>
+      <div className={styles['banner-links']}>
+        <P>
+          <ArrowLink
+            href="http://google.com"
+            title="Link para a página com detalhes sobre mim"
+          >
+            leia mais sobre mim →
+          </ArrowLink>
+        </P>
+        <P>
+          <ArrowLink href="https://twitter.com/fernahh">
+            @fernahh no twitter →
+          </ArrowLink>
+        </P>
+      </div>
     </div>
   </section>
 )
