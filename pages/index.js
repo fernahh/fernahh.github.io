12c9abc68debca3d01a88113573b57913ee94173
fernahh <fernahh@gmail.com>
2020-12-07 20:51:10 -0300

    Add newsletter-form in index page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import { Banner } from 'components/banner'
 import { Page } from 'components/page'
 import { PostContentSection } from 'components/post-content-section'
 import { TalksContentSection } from 'components/talks-content-section'
+import { NewsletterForm } from 'components/newsletter-form'
 
 const Index = () => (
   <Page fullWidth>
     <Banner />
     <PostContentSection />
     <TalksContentSection />
+    <NewsletterForm />
   </Page>
 )
 
